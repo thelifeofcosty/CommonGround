fda56529c4da65ae4d9afd40e00e852f4044f098
thelifeofcosty <70379360+thelifeofcosty@users.noreply.github.com>
2026-03-23 17:32:59 +0000

    Fix font file paths in index.css

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    jsxRuntime: 'automatic',
-  })],
+  plugins: [react()],
   server: {
     port: 3000,
-  },
-  esbuild: {
-    jsxFactory: 'React.createElement',
-    jsxFragment: 'React.Fragment',
   },
 })
